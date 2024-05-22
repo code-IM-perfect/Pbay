34cf9250ccc60242b0a7f37309b03d3ebf18e27f
Harshit Tomar <harshitt345@gmail.com>
2024-05-22 17:16:09 +0530

    Frontend part of product card on marketplace

--- src/components/+ProductMarket.svelte
+++ src/components/+ProductMarket.svelte
@@ -1,13 +1,48 @@
-<div class="cardContainer grid grid-cols-2 text-white h-[35vh] border-2">
-	<div class="image border-2 col-span-2">Image Goes here</div>
-	<h3 class="col-span-2">₹Price</h3>
-	<h4 class="col-span-2">Product Name</h4>
-	<p>Category</p>
-	<p>Time</p>
+<script>
+	export let img = '/src/lib/assets/placeholder/1.jpg';
+	export let name = 'Product Name';
+	export let price = 'Price';
+	export let seller = 'Seller';
+	export let time = 'Time';
+
+	// let glowBlur = 12;
+	// let glowRange = 8;
+	// let glowOpacity = 0.5;
+</script>
+
+<div
+	class="cardContainer hover:bg-white hover:text-dark rounded-xl grid text-white h-full min-h-96 w-full max-w-96"
+	style=""
+>
+	<div
+		class="image rounded-xl m-4 h-72 bg-center bg-no-repeat bg-cover"
+		style="background-image:url({img}); align-self:normal;"
+	></div>
+	<h4 class="text-xl font-medium mx-6 my-3">{name}</h4>
+	<h3 class="text-4xl font-bold mx-6">₹{price}</h3>
+	<div class="lastRow flex justify-between mx-6 my-6">
+		<p>{seller}</p>
+		<p class="justify-self-end">{time}</p>
+	</div>
 </div>
 
 <style>
 	.cardContainer {
-		grid-template-rows: 9fr 2fr 2fr 1fr;
+		/* grid-template-rows: 12fr auto auto 1fr; */
+		/* grid-template-rows: subgrid; */
+		align-items: center;
+		/* grid-row: 1/5; */
+		/* background-color: #111; */
+		box-shadow: 0px 0px 14px 10px rgba(0, 0, 0, 0.6);
+		transition: all 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+	}
+
+	.cardContainer:hover {
+		box-shadow: 0px 0px 40px 15px rgba(255, 255, 255, 0.5);
+		transform: scale(1.1);
+	}
+
+	h3 {
+		align-self: baseline;
 	}
 </style>
--- src/routes/market/+page.svelte
+++ src/routes/market/+page.svelte
@@ -2,11 +2,11 @@
 	import ProductMarket from '../../components/+ProductMarket.svelte';
 </script>
 
-<h1 class="text-center text-white text-4xl m-10 mt-20">Explore Products!</h1>
+<h1 class="text-center text-white font-medium text-5xl m-10 mt-20">Explore Products!</h1>
 
-<section class="prodContainer m-40 grid grid-cols-4 gap-24">
+<section class="prodContainer grid gap-24 place-items-center">
 	<ProductMarket />
-	<ProductMarket />
+	<ProductMarket name="This is a long 2 line name which can be handled" />
 	<ProductMarket />
 	<ProductMarket />
 	<ProductMarket />
@@ -15,4 +15,8 @@
 </section>
 
 <style>
+	.prodContainer {
+		grid-template-columns: repeat(auto-fit, minmax(17rem, 1fr));
+		margin: 10rem clamp(5rem, 10rem, 50vw);
+	}
 </style>
